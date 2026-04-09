75fd1b9056caf71bbb96f508d67290e94843f8f8
vincentlalo-long <nickmoi3056@gmail.com>
2026-04-10 00:30:32 +0700

    feat : add application.yaml and test basic

---
+++ backend/src/test/java/com/kstn/group4/backend/ConfigCheckTest.java
@@ -0,0 +1,25 @@
+package com.kstn.group4.backend;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigCheckTest {
+
+    @Value("${spring.application.name}")
+    private String appName;
+
+    @Value("${application.security.jwt.secret-key}")
+    private String jwtSecret;
+
+    @Test
+    void verifyYamlIsWorking() {
+        System.out.println(" Tên ứng dụng đang đọc từ YAML: " + appName);
+        System.out.println("JWT Secret đang đọc từ YAML: " + jwtSecret);
+
+        assertThat(appName).isEqualTo("football-system");
+        assertThat(jwtSecret).isNotNull();
+    }
+}
